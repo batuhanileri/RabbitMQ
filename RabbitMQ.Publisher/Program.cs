087ba9772912f63374866f94515d5fd286dc2ff9
batuhanileri <bileriofficial@gmail.com>
2025-01-13 16:15:26 +0300

    publisher create connection and send message

--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using System.Text;
+
+// Bağlantı oluşturma
+ConnectionFactory factory = new()
+{
+    Uri = new Uri("ampq.com")
+};
+
+// Bağlantıyı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+//Queue Mesaj gönderme
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları byte dönüştürmemiz gerekmektedir.
+
+byte[] message = Encoding.UTF8.GetBytes("Merhaba");
+channel.BasicPublish(exchange:"", routingKey: "example-queue", body: message);
+
+Console.Read();
